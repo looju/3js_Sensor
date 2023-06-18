7f538e4d26fcaa824132ec7a770ef5eb35cc620c
looju <omofade2020@gmail.com>
2023-06-18 12:03:45 +0100

    ref animation

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { PointLightShadow } from "three";
+import { useState, useRef } from "react";
 
 export default function App() {
+  function Box(props) {
+    const [active, setActive] = useState(false);
+    const ref = useRef();
+    useFrame((state, delta, xrFrame) => {
+      ref.current.rotation.y += delta;
+    });
+    return (
+      <mesh
+        {...props}
+        scale={active ? 1.5 : 1.0}
+        ref={ref}
+        onClick={() => setActive(!active)}
+      >
+        <boxGeometry />
+        <meshToonMaterial color={active ? "pink" : "orange"} />
+      </mesh>
+    );
+  }
+
   return (
     <Canvas>
       <ambientLight intensity={1} />
       <pointLight position={[10, 10, 10]} castShadow />
-      <mesh>
-        <sphereGeometry />
-        <meshToonMaterial color={"orange"} />
-      </mesh>
-      <mesh position={[-1, -3, -1]}>
-        <torusGeometry  />
-        <meshPhysicalMaterial color={"purple"} />
-      </mesh>
+      <Box />
+      <Box position={[0, -2, 0]} />
     </Canvas>
   );
 }
