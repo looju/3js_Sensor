3be83fc2eb59e5f5fd6461cfed7eb9a13c4f913a
looju <omofade2020@gmail.com>
2023-06-18 18:52:26 +0100

    obj loader

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { PointLightShadow } from "three";
-import { useState, useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { useState, useRef, Suspense } from "react";
 
 export default function App() {
   function Box(props) {
@@ -8,7 +8,7 @@ export default function App() {
     const ref = useRef();
     useFrame((state, delta, xrFrame) => {
       if (active) {
-        ref.current.rotation.x -= delta *2;
+        ref.current.rotation.x -= delta * 2;
       }
     });
     return (
@@ -24,12 +24,26 @@ export default function App() {
     );
   }
 
+  function Shoe(props) {
+    const obj = useLoader(
+      OBJLoader,
+      require("./assets/Asset Bundle/Airmax/shoe.obj")
+    );
+    return (
+      <mesh>
+        <primitive object={obj} />
+      </mesh>
+    );
+  }
+
   return (
     <Canvas>
       <ambientLight intensity={1} />
       <pointLight position={[10, 10, 10]} castShadow />
-      <Box />
-      <Box position={[0, -2, 0]} />
+      <Suspense fallback={null}>
+        <Shoe />
+      </Suspense>
+      <Shoe />
     </Canvas>
   );
 }
